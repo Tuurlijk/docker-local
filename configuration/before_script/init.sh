d64e222c8b208dff00f1f93e8c57686fa3508a7a
Michiel Roos <michiel@michielroos.com>
2019-04-17 08:03:55 +0200

    Ensure typo3conf exists, fix permissions for whole project root

--- configuration/before_script/init.sh
+++ configuration/before_script/init.sh
@@ -8,6 +8,9 @@ function e_success() { echo -e "\033[1;32m✔\033[0m $@"; }
 
 e_header Setting up TYPO3
 
+e_arrow Ensuring typo3conf exists
+mkdir -p /build/Web/typo3conf/
+
 e_arrow Populating additional typo3conf files
 cp --recursive --force /configuration/before_script/typo3conf/* /build/Web/typo3conf/
 
@@ -15,7 +18,7 @@ e_arrow Enabling install tool
 touch /build/Web/typo3conf/ENABLE_INSTALL_TOOL
 
 e_arrow Fixing permissions
-chmod -R ug+rwX,o+rX /build/vendor /build/Web
+chmod -R ug+rwX,o+rX /build
 chown -R 1000:1000 /build/Web/typo3conf
 
 e_success All done
