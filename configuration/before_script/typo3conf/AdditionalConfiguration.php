8e2b6fb7f526dd5b914789f72117d65bd629c281
Michiel Roos <michiel@michielroos.com>
2019-04-19 09:09:23 +0200

    Code cleanup

--- configuration/before_script/typo3conf/AdditionalConfiguration.php
+++ configuration/before_script/typo3conf/AdditionalConfiguration.php
@@ -6,7 +6,7 @@ use Symfony\Component\Dotenv\Dotenv;
 $dotenv = new Dotenv();
 $dotenv->load(__DIR__ . '/../../.env');
 
-$GLOBALS['TYPO3_CONF_VARS']['BE']['lockSSL'] = false;
+$GLOBALS['TYPO3_CONF_VARS']['BE']['lockSSL'] = true;
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] = getenv('TYPO3_DB_HOST');
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = getenv('TYPO3_DB_DATABASE');
 $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = getenv('TYPO3_DB_USER');
@@ -14,14 +14,16 @@ $GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] = getenv
 
 # Custom local dev configuration
 $GLOBALS['TYPO3_CONF_VARS']['BE']['debug'] = 1;
+//$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['backendFavicon'] = 'EXT:site_michiel/Resources/Public/Icon/develop.ico';
+//$GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['backendLogo'] = 'EXT:site_michiel/Resources/Public/Icon/develop.svg';
 $GLOBALS['TYPO3_CONF_VARS']['EXTENSIONS']['backend']['loginHighlightColor'] = '#52901b';
 $GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] = 1;
 $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport'] = 'smtp';
 $GLOBALS['TYPO3_CONF_VARS']['MAIL']['transport_smtp_server'] = 'mailhog:1025';
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['cookieSecure'] = 0;
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['cookieSecure'] = 1;
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'] = '*';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['displayErrors'] = 1;
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fileCreateMask'] = '0664';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['folderCreateMask'] = '0775';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['sitename'] .= ' - local';
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern'] = '*';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['trustedHostsPattern'] = '.*';
---
+++ configuration/bin/dump.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Echo functions
+function e_header()  { echo -e "\n\033[1m$@\033[0m"; }
+function e_success() { echo -e "\033[1;32m✔\033[0m $@"; }
+function e_error()   { echo -e "\033[1;31m✖\033[0m $@"; }
+function e_arrow()   { echo -e "\033[1;34m➜\033[0m $@"; }
+function e_arrow_n() { echo -n -e "\033[1;34m➜\033[0m $@"; }
+
+projectRoot=`dirname $0`
+projectRoot=`readlink -f $projectRoot`
+projectRoot=`dirname $projectRoot`
+projectRoot=`dirname $projectRoot`
+
+source "$projectRoot/.env"
+
+dumpPath="$projectRoot/${CONFIGURATION_ROOT}/db"
+dumpFile=${dumpPath}/dump.sql
+db=${MYSQL_DATABASE}
+
+e_header Dumping database ${db}
+e_arrow Creating backup file from current dump
+if [[ -e ${dumpFile}.gz ]]; then
+    mv ${dumpFile}.gz ${dumpFile}.gz.bak
+fi
+docker-compose exec db mysqldump -p${MYSQL_ROOT_PASSWORD} ${db} > ${dumpFile}
+if [[ $? -gt 0 ]]; then
+    e_error Failed to create backup
+    e_arrow Restoring backup file to dump
+    mv ${dumpFile}.gz.bak ${dumpFile}.gz
+    e_arrow Removing failed dump file
+    rm ${dumpFile}
+else
+#    e_arrow Removing first line from dump
+#    sed -i '1d' ${dumpFile}
+    e_arrow Gzipping dump
+    gzip ${dumpFile}
+    e_arrow Fixing permissions
+    chmod o+r ${dumpFile}.gz
+fi
