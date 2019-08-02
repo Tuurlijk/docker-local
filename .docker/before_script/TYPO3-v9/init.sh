32915df751c82b2356334d71e2853ff78ef9ce4f
Michiel Roos <michiel@michielroos.com>
2019-08-02 10:00:10 +0200

    Make template choosing easier

---
+++ .docker/before_script/TYPO3-v9/init.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Initialization script with which you can set up your project
+
+function e_header()  { echo -e "\n\033[1m$@\033[0m"; }
+function e_arrow()   { echo -e "\033[1;34m➜\033[0m $@"; }
+function e_success() { echo -e "\033[1;32m✔\033[0m $@"; }
+
+e_header Setting up TYPO3 9
+
+e_arrow Ensuring typo3conf exists
+mkdir -p /build/Web/typo3conf/
+
+e_arrow Ensuring var exists
+mkdir -p /build/var
+
+e_arrow Populating additional typo3conf files
+cp --recursive --force /configuration/before_script/TYPO3-v9/typo3conf/* /build/Web/typo3conf/
+
+e_arrow Setting up composer file
+cp --force /configuration/before_script/TYPO3-v9/composer.json /build/
+
+e_arrow Enabling install tool
+touch /build/Web/typo3conf/ENABLE_INSTALL_TOOL
+
+if [ ! -f /build/Web/typo3conf/AdditionalConfiguration.php ]; then
+  e_arrow Creating FIRST_INSTALL file
+  touch /build/Web/FIRST_INSTALL
+fi
+
+e_arrow Fixing permissions
+chmod -R ug+rwX,o+rX /build
+chown -R 1000:1000 /build
+e_success All done
---
+++ .docker/before_script/default/init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Initialization script with which you can set up your project
+
+function e_header()  { echo -e "\n\033[1m$@\033[0m"; }
+function e_arrow()   { echo -e "\033[1;34m➜\033[0m $@"; }
+function e_success() { echo -e "\033[1;32m✔\033[0m $@"; }
+
+e_header Setting up default
+
+e_arrow Creating index.php
+cp --force /configuration/before_script/default/index.php /build/Web/
+
+e_arrow Fixing permissions
+chmod -R ug+rwX,o+rX /build
+chown -R 1000:1000 /build
+e_success All done
