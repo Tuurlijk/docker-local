40b7408d68f0ae84e71e6c3e707cc066fffb16cf
Michiel Roos <michiel@michielroos.com>
2019-08-09 19:35:28 +0200

    PHP: Use pre-built images

--- build/dev.sh
+++
@@ -1,6 +0,0 @@
-# Colorful messages
-e_header()  { echo -e "\n\033[1m$@\033[0m"; }
-e_success() { echo -e " \033[1;32m✔\033[0m  $@"; }
-e_error()   { echo -e " \033[1;31m✖\033[0m  $@"; }
-
-export OPENSSL_CONF=/etc/ssl/
