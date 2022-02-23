399e10d6a9cf3167e332a3ae0e4fab1045a1bcd3
Michiel Roos <michiel@michielroos.com>
2022-02-23 15:02:15 +0100

    Nginx: replace COMPOSE_PROJECT_NAME env var in nginx configuration

--- before/before.sh
+++ before/before.sh
@@ -23,4 +23,10 @@ cat /tmp/group > /etc/group
 awk -F: -v OFS=: '$1 ~ "^dev:" { $3=ENVIRON["GROUP_ID"] }1' /etc/group- > /tmp/group-
 cat /tmp/group- > /etc/group-
 
+e_header Setting up nginx default configuration
+cp /configuration/web/nginx.default.conf.tmpl /configuration/web/nginx.default.conf
+sed -i "s/\${COMPOSE_PROJECT_NAME}/${COMPOSE_PROJECT_NAME}/g" /configuration/web/nginx.default.conf
+cp /configuration/web/nginx.conf.tmpl /configuration/web/nginx.conf
+sed -i "s/\${COMPOSE_PROJECT_NAME}/${COMPOSE_PROJECT_NAME}/g" /configuration/web/nginx.conf
+
 e_success All done
